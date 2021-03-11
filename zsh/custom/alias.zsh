fa49a596c36348acfa21a413b90cad379c60c416
Wassim DHIF <wassimdhif@gmail.com>
2021-03-11 20:58:51 +0100

    [zsh] Fix ls colors

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -8,6 +8,13 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+# Detect which 'ls' flavor is in use
+if [[ $OSTYPE == 'linux-gnu' ]]; then
+  colorflag='--color'
+elif [[ $OSTYPE == 'darwin'* ]]; then
+  colorflag="-G"
+fi
+
 # ls aliases
 alias ls="ls ${colorflag}"
 alias ll="ls -alF"
--- zsh/custom/settings.zsh
+++ zsh/custom/settings.zsh
@@ -2,13 +2,6 @@
 # General settings #
 ####################
 
-# Detect which 'ls' flavor is in use
-if [[ $OSTYPE == 'linux-gnu' ]]; then
-  colorflag='--color'
-elif [[ $OSTYPE == 'darwin'* ]]; then
-  colorflag="-G"
-fi
-
 # Add macOS style navigation
 if [[ $OSTYPE == 'darwin'* ]]; then
   bindkey "[D" backward-word

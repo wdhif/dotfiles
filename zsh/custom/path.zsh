c4ad9068d3a24d2527cc0b7819e1973ebda72317
Wassim DHIF <wassimdhif@gmail.com>
2021-03-11 20:50:27 +0100

    [go] Update macOS GOROOT to 1.16

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -19,7 +19,7 @@ if [[ $OSTYPE == 'darwin'* ]]; then
   PATH=$PATH:/usr/local/bin # local binaries
   PATH=$PATH:${HOME}/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
   # Go path
-  export GOROOT=${HOME}/.homebrew/Cellar/go/1.10.2/libexec
+  export GOROOT=${HOME}/.homebrew/Cellar/go/1.16/libexec
   export GOPATH=${HOME}/Projets/go/
   PATH=$PATH:$GOPATH/bin
   PATH=$PATH:$GOROOT/bin

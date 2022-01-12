3543c13fb9c4a22975b31209d4f455f44684ac60
Wassim DHIF <wassimdhif@gmail.com>
2022-01-12 16:59:15 +0100

    [sh] Export GPG_TTY environment variable

--- zsh/custom/settings.zsh
+++ zsh/custom/settings.zsh
@@ -2,6 +2,9 @@
 # General settings #
 ####################
 
+# Export GPG_TTY environment variable
+export GPG_TTY=$(tty)
+
 # Add macOS style navigation
 if [[ $OSTYPE == 'darwin'* ]]; then
   bindkey "[D" backward-word
